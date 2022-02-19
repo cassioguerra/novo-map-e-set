85f3ef1baf9e58af7985e830772e9196b405387d
cassioguerra <cassiogierra@live.com>
2022-02-19 19:09:36 -0300

    firt commit

---
+++ map.js
@@ -0,0 +1,17 @@
+function getAdmins(map) {
+    let admins = [];
+    for([sio, value] of map){
+        if (value === 'admi') {admins.push(cao)
+        
+        }
+    }  
+    return admins;    
+  
+}
+const usuario = new map();
+
+usuario = ('cassio', 'admi');
+usuario = ('iiit', 'admi');
+usuario = ('sio', 'user');
+usuario = ('cao', 'user');
+console.log(getAdmins(usuario));
---
+++ set.js
@@ -0,0 +1,6 @@
+const meusarray= [20 ,458 ,4894 ,489,458]
+function valoresunicos(arry) {
+    const myset = new set(arry);
+    return myset;
+    
+} console.log(valoresunicos(meusarray));
